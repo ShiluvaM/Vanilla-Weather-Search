361020c61ec927d6008f2b15c757238798ff0c11
ShiluvaM <shiluva.madingana@gmail.com>
2024-09-23 01:45:36 +0200

    Added apiUrl and apiKey

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apikey = b842bfbb6f04ee3730ao4ete0a9beb29;
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchformElement = document.querySelector("#search-form");
