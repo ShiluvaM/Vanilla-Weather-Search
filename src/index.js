01e1d1f145a6feed593a234e939cdf92487190a1
ShiluvaM <shiluva.madingana@gmail.com>
2024-09-23 01:50:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function searchCity(city) {
-  let apikey = b842bfbb6f04ee3730ao4ete0a9beb29;
+  let apiKey = b842bfbb6f04ee3730ao4ete0a9beb29;
   let apiUrl =
     "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
   console.log(apiUrl);
