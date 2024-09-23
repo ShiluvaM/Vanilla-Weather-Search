7311a40976a2fa8a958f6739695f4b1d3cca21ab
ShiluvaM <shiluva.madingana@gmail.com>
2024-09-23 02:10:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.temperature.current;
+  let currentTemperature = response.data.temperature.current;
 
-  temperatureElement.innerHTML = Math.round(temperature);
+  temperatureElement.innerHTML = Math.round(currentTemperature);
 }
 
 function searchCity(city) {
-  let apiKey = b842bfbb6f04ee3730ao4ete0a9beb29;
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
-  axiois.get(apiUrl).then(refreshWeather);
+  let apiKey = "b842bfbb6f04ee3730ao4ete0a9beb29";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
